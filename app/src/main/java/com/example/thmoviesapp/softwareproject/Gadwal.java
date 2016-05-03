fdfe0aa6830f1290203a41941edbad84f3e76d9e
Abed Eid <abedeidgithub@gmail.com>
2016-05-03 22:08:30 +0200

    "V 1.2"

--- app/src/main/java/com/example/thmoviesapp/softwareproject/Gadwal.java
+++ app/src/main/java/com/example/thmoviesapp/softwareproject/Gadwal.java
@@ -4,96 +4,155 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Abed Eid on 01/05/2016.
  */
 public class Gadwal extends Fragment {
-    Firebase firebase;
-    String stringday;
+    Firebase firebase,schedule_doctor;
+    String d,d2;
     String Group, Year, Section;
-    ArrayList<Student> students;
-    Student s;
+    List<String> dlist;
+    Gadwal_adpter gadwal_adpter;
     public static final String DEFAULT = "default";
-
-    public void Gadwal() {
-
-    }
-
+    ListView list;
+String day;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        Calendar c = Calendar.getInstance();
-        int seconds = c.get(Calendar.SECOND);
-        int minute = c.get(Calendar.MINUTE);
-        int Hour = c.get(Calendar.HOUR);
-        int day = c.get(Calendar.DAY_OF_WEEK);
-        if (day == 1) {
-            stringday = "sunday";
-        } else if (day == 2) {
-
-            stringday = "monday";
-        } else if (day == 3) {
-
-            stringday = "tuesday";
-        } else if (day == 4) {
-
-            stringday = "wednesday";
-        } else if (day == 5) {
-
-            stringday = "thursday";
-        }
-
         SharedPreferences sharedPreferences = getActivity().getSharedPreferences("Data", Context.MODE_PRIVATE);
         Group = sharedPreferences.getString("Group", DEFAULT);
         Section = sharedPreferences.getString("Section", DEFAULT);
         Year = sharedPreferences.getString("Year", DEFAULT);
+        firebase = new Firebase("https://torrid-torch-3608.firebaseio.com/assistant");
+        schedule_doctor = new Firebase("https://torrid-torch-3608.firebaseio.com/schedule_doctor");
 
+        dlist = new ArrayList<String>();
+        list = (ListView) getActivity().findViewById(R.id.list3);
+        Calendar c = Calendar.getInstance();
+        int MINUTE = c.get(Calendar.MINUTE);
+        int HOUR=c.get(Calendar.HOUR);
+        int Day=c.get(Calendar.DAY_OF_WEEK);
+        if (Day == 2) day = "monday";
+        else if (Day== 3) day = "tuesday";
+        else if (Day == 4) day = "wednesday";
+        else if (Day == 5) day = "thursday";
+        else if (Day == 6) day = "friday";
+        else if (Day == 7) day = "saturday";
+        else if (Day == 1) day = "sunday";
 
-      //  Toast.makeText(getActivity(), Hour + ": " + minute, Toast.LENGTH_SHORT).show();
-//        firebase = new Firebase("https://torrid-torch-3608.firebaseio.com/schedule_assistant");
-//        s = new Student();
-//        firebase.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                Log.d("hiiiiiiii", " " + dataSnapshot.getValue());
-//                for (int i = 0; i < dataSnapshot.getChildrenCount(); i++) {
-//                    Map<String, String> m = dataSnapshot.child(i + "").getValue(Map.class);
-//                    //section
-//                    if (m.get("day").equals(stringday.trim()) && m.get("group").equals(Group) && m.get("year").equals(Year) && m.get("section").equals(Section))
-//                        //Log.d("hiiiiiiii", "" +  +" "++" "++" "++);
-//                        s.setBegin(m.get("begin"));
-//                    s.setSubject(m.get("subject"));
-//                    s.setPlace(m.get("place"));
-//                    s.setEnd(m.get("end"));
-//                    s.setDoctor(m.get("doctor"));
-//                    students.add(s);
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(FirebaseError firebaseError) {
-//
-//            }
-//        });
 
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_gadwal, container, false);
+        final View b = inflater.inflate(R.layout.fragment_gadwal, container, false);
 
+        list = (ListView) b.findViewById(R.id.list3);
+        Firebase notification = firebase.child("");
+        Firebase doctor = schedule_doctor.child("");
+
+        ChildEventListener childEventListener = notification.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Map<String, String> m = dataSnapshot.getValue(Map.class);
+                if (m.get("group").equals(Group) && m.get("year").equals(Year) && m.get("section").equals(Section) && m.get("day").toString().equals("sunday")) {
+                    d = ((m.get("name").toString()) + " doctor " + (m.get("subject").toString()) + " Subject " + (m.get("begin").toString()) + " begin " + (m.get("end").toString()) + " end " + (m.get("hall").toString()) + " place");
+                    dlist.add(d);
+                    gadwal_adpter.notifyDataSetChanged();
+
+                }
+
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
+
+
+        ChildEventListener childEventListener1 = doctor.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+
+                Map<String, String> m = dataSnapshot.getValue(Map.class);
+                if (m.get("group").equals(Group) && m.get("year").equals(Year) && m.get("day").toString().equals("sunday")) {
+//                    d2 = ((m.get("doctor").toString()) + " doctor " +  " Subject " + " begin " + " end " + " place");
+                    d2 = ((m.get("doctor").toString()) + " doctor " + (m.get("subject").toString()) + " Subject " + (m.get("begin").toString()) + " begin "  + (m.get("end").toString()) + " end " + (m.get("place").toString()) + " place");
+                    Log.d("ss",d2);
+                    dlist.add(d2);
+                    gadwal_adpter.notifyDataSetChanged();
+
+                }
+
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+            }
+        });
+
+        gadwal_adpter = new Gadwal_adpter(getActivity(), dlist);
+        list.setAdapter(gadwal_adpter);
+
+
+        return b;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        dlist.clear();
     }
 }
--- app/src/main/java/com/example/thmoviesapp/softwareproject/Gadwal_adpter.java
+++ app/src/main/java/com/example/thmoviesapp/softwareproject/Gadwal_adpter.java
@@ -1,53 +1,74 @@
 package com.example.thmoviesapp.softwareproject;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
 /**
  * Created by Abed Eid on 03/05/2016.
  */
-//public class Gadwal_adpter extends BaseAdapter {
-//    private Context context;
-//    private LayoutInflater inflater;
-//    private List<String> Items;
-//    String m;
-//
-//
-//    public Gadwal_adpter(Context context, List<String> Items) {
-//        this.context = context;
-//        this.Items = Items;
-//
-//    }
-//
-//    @Override
-//    public int getCount() {
-//        return Items.size();
-//    }
-//
-//    @Override
-//    public Object getItem(int location) {
-//        return Items.get(location);
-//    }
-//
-//    @Override
-//    public long getItemId(int position) {
-//        return position;
-//    }
-//
-//
-//    @Override
-//    public View getView(int position, View convertView, ViewGroup parent) {
-//
-//        if (inflater == null)
-//            inflater = (LayoutInflater) context
-//                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-//        if (convertView == null)
-//            convertView = inflater.inflate(R.layout.gadwal_item, parent, false);
-//        TextView doctor = (TextView) convertView.findViewById(R.id._name);
-//        m = Items.get(position);
-//        doctor.setText(m.toString());
-//
-//
-//        return convertView;
-//    }
-//
-//
-//
-//}
+public class Gadwal_adpter extends BaseAdapter {
+    private Context context;
+    private LayoutInflater inflater;
+    private List<String> Items;
+    String m;
+
+
+    public Gadwal_adpter(Context context, List<String> Items) {
+        this.context = context;
+        this.Items = Items;
+
+    }
+
+    @Override
+    public int getCount() {
+        return Items.size();
+    }
+
+    @Override
+    public Object getItem(int location) {
+        return Items.get(location);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        if (inflater == null)
+            inflater = (LayoutInflater) context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (convertView == null)
+            convertView = inflater.inflate(R.layout.gadwal_item, parent, false);
+        TextView username = (TextView) convertView.findViewById(R.id.Dg_name);
+        TextView Subject = (TextView) convertView.findViewById(R.id.Dg_subject_gadwal);
+        TextView time = (TextView) convertView.findViewById(R.id.Dg_time);
+        TextView begain = (TextView) convertView.findViewById(R.id.Dg_Begin_time);
+        TextView end = (TextView) convertView.findViewById(R.id.Dg_End_time);
+        TextView place = (TextView) convertView.findViewById(R.id.Dg_place);
+        m = Items.get(position);
+        String[] parts = (m.split(" doctor"));
+        username.setText(parts[0]);
+        String[] part = parts[1].split("Subject ");
+        Subject.setText(part[0].toString());
+        String[] par = part[1].split("begin");
+        begain.setText(par[0]);
+        String[] pa = par[1].split("end");
+        end.setText(pa[0]);
+        time.setText("" + (Double.parseDouble(pa[0]) - Double.parseDouble(par[0])));
+        String[] p1 = pa[1].split("place");
+        place.setText(p1[0]);
+        return convertView;
+    }
+
+
+}
